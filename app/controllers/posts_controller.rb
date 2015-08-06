edacc92a56e06a7e5cf8e45fe0b7a194f51f0dcf
Ian Civgin <imciv2@gmail.com>
2015-08-05 17:33:56 -0700

    init commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,20 @@
+class PostsController < ApplicationController
+  
+  def index
+  end
+
+  def new
+  	@post = Post.new
+  end
+
+  def create
+  	post = Post.create(post_params)
+  	redirect_to root_path
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :password)
+    end
+
+end
